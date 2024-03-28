93b96c0bc36671f98ad5230312c395387ec9cee7
Gods-Favorite-Idiot <kingshukdas187@gmail.com>
2024-03-29 02:38:09 +0530

    (routes and config)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+    {
+        path: '',
+        loadComponent: () => import('./login/login.component')
+        .then(mod => mod.LoginComponent)
+    }, 
+    {
+        path: '**',
+        loadComponent: () => import('./login/login.component')
+        .then(mod => mod.LoginComponent)
+    }, 
+    {
+        path: 'landing',
+        loadComponent: () => import('./landing/landing.component')
+        .then(mod => mod.LandingComponent)
+    }, 
+    {
+        path: 'register-user',
+        loadComponent: () => import('./register/register.component')
+        .then(mod => mod.RegisterComponent)
+    }, 
+    {
+        path: 'add',
+        loadComponent: () => import('./add-edit/add-edit.component')
+        .then(mod => mod.AddEditComponent)
+    }, 
+    {
+        path: 'edit',
+        loadComponent: () => import('./add-edit/add-edit.component')
+        .then(mod => mod.AddEditComponent)
+    } 
+];
