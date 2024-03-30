2bc966a334700ed4cb35bca7e3eeb92bc75067af
Gods-Favorite-Idiot <kingshukdas187@gmail.com>
2024-03-31 03:46:46 +0530

    (feature) product service

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ListProductPayload, ListProductResponse } from './app.model';
+import { AddProductPayload, AddProductResponse, ListProductPayload, ListProductResponse, UpdateProductPayload, UpdateProductResponse, UploadImageResponse } from './app.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,17 @@ export class ProductService {
 
   getAllProducts(payload: ListProductPayload) {
     return this.http.post<ListProductResponse>(this.productUrl, payload);
+  }
+
+  uploadImage(payload: FormData) {
+    return this.http.post<UploadImageResponse>(this.uploadImgUrl, payload);
+  }
+
+  addProduct(payload: AddProductPayload) {
+    return this.http.post<AddProductResponse>(this.productUrl, payload);
+  }
+
+  updateProduct(payload: UpdateProductPayload) {
+    return this.http.put<UpdateProductResponse>(this.productUrl, payload);
   }
 }
